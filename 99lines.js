a9131c56560140405ac9d5e90bac0b48af245e03
Student <student@Admins-iMac.local>
2019-02-26 18:26:24 -0600

    99lines

---
+++ 99lines.js
@@ -0,0 +1,18 @@
+var friends = ["John", "James", "Cam", "David", "Matt"]
+
+
+
+for (i = 0; i < friends.length; i++) {
+
+
+    for (j = 99; j > 0; j--) {
+        if (j > 1) {
+            console.log(j + "lines of code in the file," + j + "lines of code" + friends[i] + "strikes one out, clears it all out, lines of code in the file")
+        }
+
+
+        else {
+        console.log(1 + "line of code in the file," + 1 + "line of code" + friends[i] + "strikes me out, clears it all out, line of code in the file")}
+    }
+}
+function frog() {
